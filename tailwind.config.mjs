759a5410e1c67df22a2ea91693b68f51f39388a3
darvin <todarvinrio@gmail.com>
2026-06-02 12:57:36 +0530

    feat: add fonts and basic home page

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ['"F5.6"', "sans-serif"],
+        text: ["Montserrat", "sans-serif"],
+        mono: ['"JetBrains Mono"', "monospace"],
+      },
+    },
+  },
+};
