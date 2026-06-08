0d968d40da53428fdfe4fa76cc8fa7e0d1a2cb18
darvin <todarvinrio@gmail.com>
2026-06-09 01:38:27 +0530

    fix: add brand color to contact

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,11 +2,32 @@ export interface ContactEntry {
   name: string;
   href: string;
   faIcon: string;
+  brandColor: string;
 }
 
 export const contacts: ContactEntry[] = [
-  { name: "Twitter", href: "https://x.com/maybeYonas", faIcon: "fa-brands fa-x-twitter" },
-  { name: "GitHub", href: "https://github.com/darvinrio", faIcon: "fa-brands fa-github" },
-  { name: "LinkedIn", href: "https://linkedin.com/in/darvinrio", faIcon: "fa-brands fa-linkedin" },
-  { name: "Telegram", href: "https://t.me/maybeyonas", faIcon: "fa-brands fa-telegram" },
+  {
+    name: "Twitter",
+    href: "https://x.com/maybeYonas",
+    faIcon: "fa-brands fa-x-twitter",
+    brandColor: "#000000",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/darvinrio",
+    faIcon: "fa-brands fa-github",
+    brandColor: "#333333",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/darvinrio",
+    faIcon: "fa-brands fa-linkedin",
+    brandColor: "#0A66C2",
+  },
+  {
+    name: "Telegram",
+    href: "https://t.me/maybeyonas",
+    faIcon: "fa-brands fa-telegram",
+    brandColor: "#0088cc",
+  },
 ];
