4619efb32541f2b8ffeaf6d736c96fe12ade068f
darvin <todarvinrio@gmail.com>
2026-06-09 01:27:15 +0530

    feat: add contacts section

---
+++ src/data/contact.ts
@@ -0,0 +1,11 @@
+export interface ContactEntry {
+  name: string;
+  href: string;
+}
+
+export const contacts: ContactEntry[] = [
+  { name: "Twitter", href: "https://x.com/maybeYonas" },
+  { name: "GitHub", href: "https://github.com/darvinrio" },
+  { name: "LinkedIn", href: "https://linkedin.com/in/darvinrio" },
+  { name: "Telegram", href: "https://t.me/maybeyonas" },
+];
