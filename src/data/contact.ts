66f8d21dd32dd3329f53cce7e337df4e42896350
darvin <todarvinrio@gmail.com>
2026-06-09 01:33:37 +0530

    refactor: replace svg with fontawesome logos

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,11 +1,12 @@
 export interface ContactEntry {
   name: string;
   href: string;
+  faIcon: string;
 }
 
 export const contacts: ContactEntry[] = [
-  { name: "Twitter", href: "https://x.com/maybeYonas" },
-  { name: "GitHub", href: "https://github.com/darvinrio" },
-  { name: "LinkedIn", href: "https://linkedin.com/in/darvinrio" },
-  { name: "Telegram", href: "https://t.me/maybeyonas" },
+  { name: "Twitter", href: "https://x.com/maybeYonas", faIcon: "fa-brands fa-x-twitter" },
+  { name: "GitHub", href: "https://github.com/darvinrio", faIcon: "fa-brands fa-github" },
+  { name: "LinkedIn", href: "https://linkedin.com/in/darvinrio", faIcon: "fa-brands fa-linkedin" },
+  { name: "Telegram", href: "https://t.me/maybeyonas", faIcon: "fa-brands fa-telegram" },
 ];
